bb4b2d5978a8f6c06880ba21dfa14ad34c65037e
Kamil Vakhitov <kamil.vakhitov95@gmail.com>
2022-12-19 21:13:39 +0500

    feat: add tabs modules

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,48 +1,50 @@
-
-
 const modals = () => {
-	function bindModal(triggerSelector, modalSelector, closeSelector) {
-		const trigger = document.querySelectorAll(triggerSelector);
-		const modal = document.querySelector(modalSelector);
-		const close = document.querySelector(closeSelector);
-		
-		trigger.forEach(item => {
-			item.addEventListener('click', (e) => {
-				if(e.target) {
-					e.preventDefault();
-				}
-	
-				modal.style.display = 'block';
-				document.body.style.overflow = 'hidden';
-				// document.body.classList.add('modal-open');
-			});
-		});
+  function bindModal(triggerSelector, modalSelector, closeSelector) {
+    const trigger = document.querySelectorAll(triggerSelector);
+    const modal = document.querySelector(modalSelector);
+    const close = document.querySelector(closeSelector);
 
-		close.addEventListener('click', () => {
-			modal.style.display = 'none';
-			document.body.style.overflow = '';
-			// document.body.classList.remove('modal-open');
-		});
+    trigger.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        if (e.target) {
+          e.preventDefault();
+        }
 
-		modal.addEventListener('click', (e) => {
-			if(e.target === modal) {
-				modal.style.display = 'none';
-				document.body.style.overflow = '';
-				// document.body.classList.remove('modal-open');
-			}
-		});
-	}
+        modal.style.display = "block";
+        document.body.style.overflow = "hidden";
+        // document.body.classList.add('modal-open');
+      });
+    });
 
-	function showModalByTime(selector, time) {
-		setTimeout(function() {
-			document.querySelector(selector).style.display = 'block';
-			document.body.style.overflow = 'hidden';
-		}, time);
-	}
+    close.addEventListener("click", () => {
+      modal.style.display = "none";
+      document.body.style.overflow = "";
+      // document.body.classList.remove('modal-open');
+    });
 
-	bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
-	bindModal('.phone_link', '.popup', '.popup .popup_close');
-	showModalByTime('.popup', 3000);
+    modal.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        modal.style.display = "none";
+        document.body.style.overflow = "";
+        // document.body.classList.remove('modal-open');
+      }
+    });
+  }
+
+  function showModalByTime(selector, time) {
+    setTimeout(function () {
+      document.querySelector(selector).style.display = "block";
+      document.body.style.overflow = "hidden";
+    }, time);
+  }
+
+  bindModal(
+    ".popup_engineer_btn",
+    ".popup_engineer",
+    ".popup_engineer .popup_close"
+  );
+  bindModal(".phone_link", ".popup", ".popup .popup_close");
+  showModalByTime(".popup", 3000);
 };
 
-export default modals; 
+export default modals;
