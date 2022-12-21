7059823fd39c7e4d6d625a6afcf355e4dd79fab8
Kamil Vakhitov <kamil.vakhitov95@gmail.com>
2022-12-21 22:22:01 +0500

    fix: modal form

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,13 +1,10 @@
-const forms = () => {
+import checkNumInputs from "./checkNumInputs";
+import closeModal from "./closeModal";
+const forms = (state) => {
   const form = document.querySelectorAll("form");
   const inputs = document.querySelectorAll("input");
-  const phoneInputs = document.querySelectorAll('input[name="user_phone"]');
 
-  phoneInputs.forEach((item) => {
-    item.addEventListener("input", () => {
-      item.value = item.value.replace(/\D/, "");
-    });
-  });
+  checkNumInputs('input[name="user_phone"]');
 
   const message = {
     loading: "Загрузка...",
@@ -31,6 +28,16 @@ const forms = () => {
     });
   };
 
+  const clearCheckBox = () => {
+    document.querySelectorAll(".checkbox").forEach((item) => {
+      item.checked = false;
+    });
+  };
+
+  const clearSelect = () => {
+    document.querySelector("#view_type").selectedIndex = 0;
+  };
+
   form.forEach((item) => {
     item.addEventListener("submit", (e) => {
       e.preventDefault();
@@ -40,6 +47,11 @@ const forms = () => {
       item.appendChild(statusMessage);
 
       const formData = new FormData(item);
+      if (item.getAttribute("data-calc") === "end") {
+        for (let key in state) {
+          formData.append(key, state[key]);
+        }
+      }
       postData("assets/server.php", formData)
         .then((res) => {
           console.log(res);
@@ -50,6 +62,12 @@ const forms = () => {
           clearInputs();
           setTimeout(() => {
             statusMessage.remove();
+            closeModal();
+            clearCheckBox();
+            clearSelect();
+            for (let key in state) {
+              delete state[key];
+            }
           }, 5000);
         });
     });
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,32 +1,68 @@
 const modals = () => {
-  function bindModal(triggerSelector, modalSelector, closeSelector) {
+  function bindModal(
+    triggerSelector,
+    modalSelector,
+    closeSelector,
+    closeClickOverlay = true
+  ) {
     const trigger = document.querySelectorAll(triggerSelector);
     const modal = document.querySelector(modalSelector);
     const close = document.querySelector(closeSelector);
+    const windows = document.querySelectorAll("[data-modal]");
+    const windowWidth = document.querySelector("#width");
+    const windowHeight = document.querySelector("#height");
 
     trigger.forEach((item) => {
       item.addEventListener("click", (e) => {
         if (e.target) {
           e.preventDefault();
         }
+        if (item.classList.contains("popup_calc_button")) {
+          if (windowWidth.value.length < 1 || windowHeight.value.length < 1) {
+            windows.forEach((elem) => {
+              if (elem.style.display === "block") {
+                const errorMessage = document.createElement("div");
+                errorMessage.textContent = "Заполните обязательные поля";
+                errorMessage.classList.add("status");
+                elem.querySelector(".popup_calc_content").append(errorMessage);
+              }
+            });
+          } else {
+            windows.forEach((item) => {
+              item.style.display = "none";
+            });
 
-        modal.style.display = "block";
-        document.body.style.overflow = "hidden";
-        // document.body.classList.add('modal-open');
+            modal.style.display = "block";
+            document.body.style.overflow = "hidden";
+          }
+        } else {
+          windows.forEach((item) => {
+            item.style.display = "none";
+          });
+
+          modal.style.display = "block";
+          document.body.style.overflow = "hidden";
+        }
       });
     });
 
     close.addEventListener("click", () => {
+      windows.forEach((item) => {
+        item.style.display = "none";
+      });
+
       modal.style.display = "none";
       document.body.style.overflow = "";
-      // document.body.classList.remove('modal-open');
     });
 
     modal.addEventListener("click", (e) => {
-      if (e.target === modal) {
+      if (e.target === modal && closeClickOverlay) {
+        windows.forEach((item) => {
+          item.style.display = "none";
+        });
+
         modal.style.display = "none";
         document.body.style.overflow = "";
-        // document.body.classList.remove('modal-open');
       }
     });
   }
@@ -44,7 +80,21 @@ const modals = () => {
     ".popup_engineer .popup_close"
   );
   bindModal(".phone_link", ".popup", ".popup .popup_close");
-  showModalByTime(".popup", 3000);
+  bindModal(".popup_calc_btn", ".popup_calc", ".popup_calc_close");
+
+  bindModal(
+    ".popup_calc_button",
+    ".popup_calc_profile",
+    ".popup_calc_profile_close",
+    false
+  );
+  bindModal(
+    ".popup_calc_profile_button",
+    ".popup_calc_end",
+    ".popup_calc_end_close",
+    false
+  );
+  // showModalByTime(".popup", 3000);
 };
 
 export default modals;
